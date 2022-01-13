232ec524e5c7891e546b09d0d1c50a7892b475bd
Jonatã Hofmann <jonatahofmann@gmail.com>
2022-01-13 12:09:24 -0200

    Criação de rota de delte

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const PORT = 8000;
 app.use(express.json());
 app.use(cors({ origin: "*" }));
+
+const file = "C:\\Logs\\log.txt"
 app.post("/", (req, res) => {
   const date = new Date();
   const {
@@ -13,17 +15,23 @@ app.post("/", (req, res) => {
   } = req;
   const string = `[${date.toLocaleDateString()} ${date.toLocaleTimeString()} - ${req.get(
     "origin"
-  )}] ${message}`;
+  )}] ${message}\n`;
 
   if (type === "log") console.log("\x1b[0m", string);
   if (type === "warn") console.log("\x1b[33m", string);
   if (type === "info") console.log("\x1b[36m", string);
   if (type === "error") console.log("\x1b[31m", string);
 
-  fs.appendFileSync("log.txt", string);
+  fs.appendFileSync(file, string);
 
   return res.send("Express + TypeScript Server");
 });
+
+app.delete("/" ,(req, res) => {
+	
+	fs.unlinkSync(file);
+	return res.status(204)
+})
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
